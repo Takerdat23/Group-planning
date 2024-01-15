80e98bf1492bd609bce81291d805e5a15e5dd6a4
Nguyễn Quang Vũ <21522801@gm.uit.edu.vn>
2024-01-15 15:45:31 +0700

    Update AuthService.js

--- src/server/AuthService.js
+++ src/server/AuthService.js
@@ -95,6 +95,51 @@ export const getUser = () => {
 
   ; 
 }; 
+//Project
+export const newProject = (projectName) => {
+  if (!socket) {
+    console.warn("Socket not connected");
+    return;
+  }
+  console.log(projectName);
+  socket.emit('newProject', projectName);
+  socket.on('serverLog', (text) => {
+    console.log(text);
+  });
+}
+
+export const getProjects = () => {
+  return new Promise((resolve, reject) => {
+    if (!socket) {
+      console.warn("Socket not connected");
+      reject("Socket not connected");
+      return;
+    }
+    socket.emit('getProject');
+    socket.on('Projects', (projects) => {
+      console.log(projects);
+      resolve(projects); // Resolve the promise with the received projects
+    });
+
+    // Listen for errors
+    socket.on('error', (error) => {
+      console.error("Socket error:", error);
+      reject(error); // Reject the promise if there's an error
+    });
+  });
+};
+
+export const deleteProject = (id) => {
+  if (!socket) {
+    console.warn("Socket not connected");
+    return;
+  }
+  console.log(id);
+  socket.emit('deleteProject', id);
+  socket.on('serverLog', (text) => {
+    console.log(text);
+  });
+}
 
 export const getSocket = () => {
   return socket; // Return the socket instance
